cd14a93f93d9fe5559f7bb5589bb2fe6bc62c2e5
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-03-03 23:08:06 +0530

    Add transaction model with schema for handling transactions

---
+++ src/models/transaction.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+
+const transactionSchema = new mongoose.Schema({
+    fromAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "From account is required"],
+        index: true, // create an index on fromAccount for faster queries
+    },
+    toAccount:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "account",
+        required: [true, "To account is required"],
+        index: true, // create an index on toAccount for faster queries 
+    },
+    status:{
+        type: String,
+        enum:{
+            values: ["PENDING","COMPLETED","FAILED","REVERSED"],
+            message: "Status must be either PENDING, COMPLETED, FAILED, or REVERSED",
+        },
+        default: "PENDING"
+    },
+    amount:{
+        type: Number,
+        required: [true, "Amount is required"],
+        min: [0, "Amount must be a positive number"]
+    },
+    idempotencyKey: {
+        type: String,
+        required: [true, "Idempotency key is required"],
+        unique: true, // ensure
+        index: true, // create an index on idempotencyKey for faster queries
+    }
+},{
+    timestamps: true
+})
