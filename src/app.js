65acb6add6f2ac559b61b1c6076d88823c6978f6
syedaftab-dev <syed.md.aftab.2027@gmail.com>
2026-03-05 23:08:51 +0530

    Add test API endpoint to confirm backend service status

--- src/app.js
+++ src/app.js
@@ -11,6 +11,11 @@ const authRouter = require('./routes/auth.routes');
 const accountRouter = require('./routes/account.routes');
 const transactionRouter = require('./routes/transaction.route');
 
+// test api
+app.get("/",(req,res)=>{
+    res.send("Backend Service is up and running")
+})
+
 // ? Routes used in the application
 app.use('/api/auth', authRouter);
 app.use('/api/accounts', accountRouter);
